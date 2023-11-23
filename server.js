c8d69dd616bb292107b9ae78af22ed570458f0b8
Sebastian Hans Weissenborn <58604870+s2seweis@users.noreply.github.com>
2023-11-23 13:22:58 +0100

    add cors

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-// const dbConnection = require('./db')
+const express = require('express');
+const cors = require('cors'); // Import the cors package
+const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(express.json());
-require("dotenv").config();
-const dotenv = require("dotenv").config();
+require('dotenv').config();
+
+// Use cors middleware to enable CORS for all routes
+app.use(cors());
 
 // ### Route for JWT Authentication
-app.use('/api/users/', require('./routes/usersRoute'))
+app.use('/api/users/', require('./routes/usersRoute'));
 
+app.use('/api/settings/', require('./routes/settingsRoute'));
 
-app.use('/api/settings/', require('./routes/settingsRoute'))
+app.get('/', (req, res) => res.send('Hello World'));
 
-
-app.get('/', (req, res) => res.send('Hello World'))
-app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
+app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`));
